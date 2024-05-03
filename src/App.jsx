3d98f7675ee01c729cbce208a4c79775758e0cbc
Janadhi Dissanayake <disja508@student.otago.ac.nz>
2024-05-03 22:46:06 +1200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
 
 
 
-  const phrases = [ "Software Developer", "ML Engineer","Quant Developer", "Data Scientist", ];
+  const phrases = [ "Network Engineer", "ML Engineer", "Systems Engineer", "Quant Researcher", ];
   const [currentPhrase, setCurrentPhrase] = useState(0);
 
   useEffect(() => {
