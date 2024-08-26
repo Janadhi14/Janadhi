9758f192629b0b0778e87cdcba96aadcc2ee8194
Janadhi Dissanayake <disja508@student.otago.ac.nz>
2024-08-26 23:09:45 +1200

    updated about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,16 +8,16 @@ function AboutMe() {
       <h2 className='subtitles' >Hello World!!</h2>
 
       <p className="fade-up" style={{ animationDelay: '0.4s' }}>
-        - I love problem solving and finding innovative solutions to complex issues through code. 👨🏽‍💻
+        - I love problem solving and finding innovative solutions to complex issues through code.👨🏽‍💻
       </p>
       <p className="fade-up" style={{ animationDelay: '0.8s' }}>
-        - I'm a life long learner with a Bachelor of Science in Computer Science and a Bachelor of Biomedical Science from the University of Otago. 📚👨🏽‍🎓
+        - I'm a life long learner with a Bachelor of Science in Computer Science and a Bachelor of Biomedical Science from the University of Otago.📚👨🏽‍🎓
       </p>
       <p className="fade-up" style={{ animationDelay: '1.2s' }}>
-        - Deeply interested in exploring the convergence between Cybersecurity and Network Security. 🤖🪪
+        - Deeply interested in exploring the convergence between Cybersecurity and Network Security.🤖🪪
       </p>
       <p className="fade-up" style={{ animationDelay: '1.6s' }}>
-        - Outside of my Education I am an avid cricket player, guitarist and sprinter. 🏏🎸🏃🏽‍♂️
+        - Outside of my Education I am an avid cricket player, guitarist and sprinter.🏏🎸🏃🏽‍♂️
       </p>
     </section>
   );
